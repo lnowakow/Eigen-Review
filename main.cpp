2de7d5c9dc9591a606f12f2f7a9cc1cac5eb55fb
Lukasz Nowakowski <lnowakow@uwo.ca>
2021-12-15 00:02:21 -0500

    check packages

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+
 #include <iostream>
+#include <eigen3/Eigen/Dense>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Eigen::MatrixXd mat(4,4);
+
+    mat << 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1;
+
+    std::cout << "Hello, World! Here is a matrix: \n" << mat << std::endl;
     return 0;
 }
